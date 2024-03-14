68eab35d51006ee8fa679f5eb97230877ab6e631
ICSDEVGROUP <161980669+ICSDEVGROUP@users.noreply.github.com>
2024-03-13 18:03:41 -0600

    Update script2.js

--- script2.js
+++ script2.js
@@ -45,7 +45,7 @@ function calculateTotal() {
     let item19Quantity = document.getElementById("item19Quantity").value;
     let item20Quantity = document.getElementById("item20Quantity").value;
     let item21Quantity = document.getElementById("item21Quantity").value;
-    let item22Quantity = document.getElementById("item21Quantity").value;
+    let item22Quantity = document.getElementById("item22Quantity").value;
 
     let subtotal = (item1Price * item1Quantity) + (item2Price * item2Quantity) + (item3Price * item3Quantity) + (item4Price * item4Quantity) + (item5Price * item5Quantity) + (item6Price * item6Quantity) + (item7Price * item7Quantity) + (item8Price * item8Quantity) + (item9Price * item9Quantity) + (item10Price * item10Quantity) + (item11Price * item11Quantity) + (item12Price * item12Quantity) + (item13Price * item13Quantity) + (item14Price * item14Quantity) + (item15Price * item15Quantity) + (item16Price * item16Quantity) + (item17Price * item17Quantity) + (item18Price * item18Quantity) + (item19Price * item19Quantity) + (item20Price * item20Quantity) + (item21Price * item21Quantity) + (item22Price * item22Quantity);
 
